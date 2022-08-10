3da245b48f3d9451228cb3298d3682879195187b
jiksonsabu <jiksonks1@gmail.com>
2022-08-10 14:12:24 +0530

    project added 1

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/tdform'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/tdform/index.html'));});
+'/dist/trform/index.html'));});
 app.listen(process.env.PORT || 8080);
